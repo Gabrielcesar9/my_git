4ed95b6da516c432624e80f95a54b37218365b06
Gabrielcesar9 <gabriel.cesar.pereira.9@gmail.com>
2020-01-31 23:20:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const Datastore = require('nedb');
-app.listen(3000, () => console.log('listening at 3000'));
+//app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
 app.use(express.json({limit:'1mb'}))
 
